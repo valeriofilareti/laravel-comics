e17b69412bdad189d914f5fba19122887292dacf
valeriofilareti <valerio.filareti@gmail.com>
2023-06-06 16:58:57 +0200

    [add] comic title, description and price

--- resources/views/details.blade.php
+++ resources/views/details.blade.php
@@ -2,14 +2,18 @@
 
 @section('details')
 
-<div class="d-flex">
+<div class="details-wrap d-flex">
 
     <div class="details-img">
         <img src="{{$comic['thumb']}}" alt="">
     </div>
 
     <div class="details-txt">
-        <h1>testo</h1>
+        <h1 class="text-center my-5">{{$comic['title']}}</h1>
+        <h3>Description</h3>
+        <p>{{$comic['description']}}</p>
+        <h3>Price</h3>
+        <h2>{{$comic['price']}}</h2>
     </div>
 
 </div>
